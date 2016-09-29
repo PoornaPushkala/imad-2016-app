58e037a13490ea6f58dbca0e1004b73c398b374c
PoornaPushkala <poornapushkala97@gmail.com>
2016-09-29 21:18:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,9 @@ var element = document.getElementById('main-text'
 );
 
 element.innerHTML = 'New Value';
+
 //move the image
+
 var img = document.getElementById('madi');
 img.onclick = function () {
   img.style.marginleft = '100px';  
